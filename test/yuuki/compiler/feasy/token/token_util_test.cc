5e95add991bc21ca268c3200874203c417a14168
Alaric Gilbert <alaricgilbert@qq.com>
2020-03-04 11:12:25 +0800

    test: switch to scoped enum

--- test/yuuki/compiler/feasy/token/token_util_test.cc
+++ test/yuuki/compiler/feasy/token/token_util_test.cc
@@ -9,19 +9,19 @@ TEST(TokenUtilTest,getTypeTest) {
     TokenType t2 = TokenUtil::getType("{");
     TokenType t3 = TokenUtil::getType("continue");
 
-    EXPECT_EQ(t0, op_ampamp);
-    EXPECT_EQ(t1, kw_public);
-    EXPECT_EQ(t2, l_brace);
-    EXPECT_EQ(t3, kw_continue);
+    EXPECT_EQ(t0, TokenType::op_ampamp);
+    EXPECT_EQ(t1, TokenType::kw_public);
+    EXPECT_EQ(t2, TokenType::l_brace);
+    EXPECT_EQ(t3, TokenType::kw_continue);
 }
 
 TEST(TokenUtilTest,getSpellTest){
 
     using namespace yuuki::compiler::feasy::token;
-    auto* sp0 = TokenUtil::getSpell(op_minuseq);
-    auto* sp1 = TokenUtil::getSpell(kw_override);
-    auto* sp2 = TokenUtil::getSpell(r_paren);
-    auto* sp3 = TokenUtil::getSpell(kw_break);
+    auto* sp0 = TokenUtil::getSpell(TokenType::op_minuseq);
+    auto* sp1 = TokenUtil::getSpell(TokenType::kw_override);
+    auto* sp2 = TokenUtil::getSpell(TokenType::r_paren);
+    auto* sp3 = TokenUtil::getSpell(TokenType::kw_break);
 
     EXPECT_STREQ(sp0,"-=");
     EXPECT_STREQ(sp1,"override");
